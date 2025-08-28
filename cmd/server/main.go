3014c4d2597268a51afa8010cc272c6a7ce408ce
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-08-28 16:11:41 +0530

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/iiitk-tools/anvil-pdf/internal/config"
+	mw "github.com/iiitk-tools/anvil-pdf/internal/middlewares"
+)
+
+func main() {
+	// load configs
+	cfg := config.MustLoad()
+
+	// middlewares
+
+	// http mux constructor
+	mainMux := http.NewServeMux()
+
+	// default endpoint - {$} makes it very specific
+	mainMux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("Welcome to Anvil PDF!"))
+
+		if err != nil {
+			slog.Error("Could not write response", slog.String("error", err.Error()))
+		}
+	})
+
+	addr := strings.Join([]string{cfg.Host, cfg.Port}, ":")
+
+	server := http.Server{
+		Addr: addr,
+		Handler: mw.CORS(mainMux),
+	}
+
+	slog.Info("Server listening on http://" + addr)
+
+	//* graceful shutdown
+	done := make(chan os.Signal, 1)
+
+	// to read interrupts
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM) 
+	// when any of the above mention signals is observed, it sent it into the 'done' channel which stop the goroutine
+
+	go func() {
+		err := server.ListenAndServe(); 
+		if err != nil && err !=  http.ErrServerClosed {
+			slog.Error("Failed to start server, " + err.Error())
+			os.Exit(1)
+		}
+	} ()
+
+	<-done
+
+	slog.Info("Shutting down the server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Error shutting down the server,", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successfully")
+}
---
+++ internal/config/config.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+type HTTPServer struct {
+	Host string 
+	Port string	
+}
+
+type Config struct {
+	Env string `mapstructure:"env"`
+	HTTPServer `mapstructure:"http_server"`
+}
+
+func MustLoad() *Config {
+	// load env files
+	if err := godotenv.Load(); err != nil {
+        slog.Warn("No .env file found or error loading it", slog.String("error", err.Error()))
+    }
+
+	configPath := os.Getenv("DEV_CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			slog.Error("Config path is not set")
+			os.Exit(1)
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		slog.Error("Config file does not exist", slog.String("config-path", configPath))
+		os.Exit(1)
+	}
+
+	v := viper.New()
+	v.SetConfigFile(configPath)
+	v.AutomaticEnv()
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := v.ReadInConfig(); err != nil {
+		slog.Error("Failed to read config file", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err != nil {
+		slog.Error("Failed to load config file", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	// set port from cmd
+	port := flag.String("port", "", "port to expose the app")
+	flag.Parse()
+	if *port != "" {
+		cfg.HTTPServer.Port = *port
+	}
+
+	return &cfg
+}
